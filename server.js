f55dd611b1ce8b76a1115ed2d271540c22287f80
Haozhi <zhi_0415@hotmail.com>
2024-01-13 15:24:23 +0800

    accepting chun xiao's changes in server.js

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ var cors = require('cors');
 
 var recipesRouter = require('./routes/recipes');
 // var usersRouter = require('./routes/users');
-<<<<<<< HEAD
-=======
 var reviewsRouter = require('./routes/reviews');
->>>>>>> 5e2725d08115c11b02b3cbb74eefb6c0e59d16a5
 
 require("dotenv").config();
 require("./client/mongo");
@@ -27,9 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/recipes', recipesRouter);
 // app.use('/users', usersRouter);
-<<<<<<< HEAD
-=======
 app.use('/reviews', reviewsRouter);
->>>>>>> 5e2725d08115c11b02b3cbb74eefb6c0e59d16a5
 
 module.exports = app;
