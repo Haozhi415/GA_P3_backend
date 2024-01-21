5741bc8ff7169674e32f70f2e9d368ba7f569434
sijin <sijincag@gmail.com>
2024-01-22 01:47:51 +0800

    Add status schema in reviews daos so that when a recipe is deleted, all the associated reviews' statuses will be set to deleted. Modified  models/recipe as well to handle status change accordingly.

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,58 +1,64 @@
 const modelReviews = require("../models/reviews");
 
 module.exports = {
-    getAllReviews,
-    createReview,
-    updateReview,
-    deleteReview,
+  getRecipeReviews,
+  createReview,
+  updateReview,
+  deleteReview,
 };
 
 // Read
 
-async function getAllReviews(req, res) {
-    try {
-        const recipeId = req.params.recipeId;
-        const reviews = await modelReviews.getAllReviewsByRecipeId(recipeId);
+async function getRecipeReviews(req, res) {
+  try {
+    const recipeId = req.params.recipeId;
+    const reviews = await modelReviews.getRecipeReviewsByRecipeId(recipeId);
 
-        res.status(200).json(reviews);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ errorMsg: error.message });
-    }
+    res.status(200).json(reviews);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ errorMsg: error.message });
+  }
 }
 
 // Create a review based on recipe
 
 async function createReview(req, res) {
-    try {
-        const review = await modelReviews.createReview(req.params.recipeId, req.body);
-        res.status(201).json(review);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ errorMsg: error.message });
-    }
+  try {
+    const review = await modelReviews.createReview(
+      req.params.recipeId,
+      req.body
+    );
+    res.status(201).json(review);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ errorMsg: error.message });
+  }
 }
 
 // Update
 
 async function updateReview(req, res) {
-    try {
-        const updatedReview = await modelReviews.updateReview(req.params.reviewId, req.body);
-        res.status(200).json(updatedReview);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ errorMsg: error.message });
-    }
+  try {
+    const updatedReview = await modelReviews.updateReview(
+      req.params.reviewId,
+      req.body
+    );
+    res.status(200).json(updatedReview);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ errorMsg: error.message });
+  }
 }
 
 // Delete
 
 async function deleteReview(req, res) {
-    try {
-        await modelReviews.deleteReview(req.params.reviewId);
-        res.status(200).json({ message: "Review deleted." });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ errorMsg: error.message });
-    }
+  try {
+    await modelReviews.deleteReview(req.params.reviewId);
+    res.status(200).json({ message: "Review deleted." });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ errorMsg: error.message });
+  }
 }
--- models/reviews.js
+++ models/reviews.js
@@ -2,10 +2,10 @@ const daoRecipes = require("../daos/recipes");
 const daoReviews = require("../daos/reviews");
 
 module.exports = {
-    getAllReviewsByRecipeId,
-    createReview,
-    updateReview,
-    deleteReview,
+  getRecipeReviewsByRecipeId,
+  createReview,
+  updateReview,
+  deleteReview,
 };
 
 // Create a review for a recipe via the recipeId.
@@ -13,112 +13,106 @@ module.exports = {
 // reviewData is req.body from controllers.
 // to pass in userId in future
 async function createReview(recipeId, reviewData) {
+  // First find the recipe with the given ID
+  // The result is stored in the "recipe" variable.
+  const recipe = await daoRecipes.findById(recipeId);
 
-    // First find the recipe with the given ID
-    // The result is stored in the "recipe" variable.
-    const recipe = await daoRecipes.findById(recipeId);
+  // If "recipe" returns null, means the recipe does not exist.
+  if (!recipe) {
+    return (message = "Recipe does not exist.");
+  }
 
-    // If "recipe" returns null, means the recipe does not exist.
-    if (!recipe) {
-        return (message = "Recipe does not exist.");
-    }
+  // If "recipe" returns truthy, means the recipe exists.
+  // Then create a new review with the provided data (following schema in daos).
+  const review = await daoReviews.create({
+    recipe: recipeId,
+    //  username: userId,
+    title: reviewData.title,
+    rating: reviewData.rating,
+    content: reviewData.content,
+    images: reviewData.images,
+  });
 
-    // If "recipe" returns truthy, means the recipe exists.
-    // Then create a new review with the provided data (following schema in daos).
-    const review = await daoReviews.create({
-        recipe: recipeId,
-      //  username: userId,
-        title: reviewData.title,
-        rating: reviewData.rating,
-        content: reviewData.content,
-        images: reviewData.images
-    });
+  // Next the following adds the _id of the newly created review to the recipe's review array.
+  // push is a Mongoose method that adds an item to an array.
+  recipe.reviews.push(review._id);
 
-    // Next the following adds the _id of the newly created review to the recipe's review array.
-    // push is a Mongoose method that adds an item to an array.
-    recipe.reviews.push(review._id);
+  // After pushing the review to the recipe's review array, save the updated recipe.
+  await recipe.save();
 
-    // After pushing the review to the recipe's review array, save the updated recipe.
-    await recipe.save();
-
-    // Return the newly created review
-    return review;
+  // Return the newly created review
+  return review;
 }
-
 
 // Delete a review via the review's ID.
 async function deleteReview(reviewId) {
+  // First find the review with the given ID.
+  const review = await daoReviews.findById(reviewId);
 
-    // First find the review with the given ID.
-    const review = await daoReviews.findById(reviewId);
+  // If "review" returns null, means the review does not exist.
+  // Else, delete the review.
+  if (!review) {
+    return (message = "Review does not exist.");
+  } else {
+    await daoReviews.findByIdAndDelete(reviewId);
+  }
 
-    // If "review" returns null, means the review does not exist.
-    // Else, delete the review.
-    if (!review) {
-        return (message = "Review does not exist.");
-    } else {
-        await daoReviews.findByIdAndDelete(reviewId);
-    }
+  // Find the recipe with the given reviewId.
+  // The result is stored in the "recipe" variable.
+  const recipe = await daoRecipes.findOne({ reviews: reviewId });
 
-    // Find the recipe with the given reviewId.
-    // The result is stored in the "recipe" variable.
-    const recipe = await daoRecipes.findOne({ reviews: reviewId });
+  // If "recipe" returns null, means the recipe does not exist.
+  // Else, delete the review from the recipe's reviews array.
+  if (!recipe) {
+    return (message = "Review does not exist.");
+  } else {
+    // Remove the reviewId from the recipe's reviews array.
+    // pull is a Mongoose method that removes an item from an array.
+    recipe.reviews.pull(reviewId);
 
-    // If "recipe" returns null, means the recipe does not exist.
-    // Else, delete the review from the recipe's reviews array.
-    if (!recipe) {
-        return (message = "Review does not exist.");
-    } else {
+    // After removing the reviewId from the recipe's review array, save the updated recipe.
+    await recipe.save();
+  }
 
-        // Remove the reviewId from the recipe's reviews array.
-        // pull is a Mongoose method that removes an item from an array.
-        recipe.reviews.pull(reviewId);
-
-        // After removing the reviewId from the recipe's review array, save the updated recipe.
-        await recipe.save();
-    }
-
-    return (message = "Review deleted.");
+  return (message = "Review deleted.");
 }
 
 // Get all reviews for a recipe by recipeId
 
-async function getAllReviewsByRecipeId(recipeId) {
-    
-    const getAllReviews = await daoReviews.find({ recipe: recipeId });
-    
-    if (!getAllReviews || getAllReviews.length === 0) {
-        return (message = "No reviews available.");
-    } 
-        return getAllReviews;
-}
+async function getRecipeReviewsByRecipeId(recipeId) {
+  const getRecipeReviews = await daoReviews.find({ recipe: recipeId });
 
+  if (!getRecipeReviews || getRecipeReviews.length === 0) {
+    return (message = "No reviews available.");
+  }
+  return getRecipeReviews;
+}
 
 // Update a review via the reviewId.
 
 async function updateReview(reviewId, reviewData) {
-    const review = await daoReviews.findById(reviewId);
+  const review = await daoReviews.findById(reviewId);
 
-    if (!review) {
-        return { message: "Review does not exist." };
-    } else {
-        await daoReviews.findByIdAndUpdate(
-        reviewId,
-        {
-            title: reviewData.title,
-            rating: reviewData.rating,
-            content: reviewData.content,
-            images: reviewData.images,
-        },
-        { new: true }
+  if (!review) {
+    return { message: "Review does not exist." };
+  } else {
+    await daoReviews.findByIdAndUpdate(
+      reviewId,
+      {
+        title: reviewData.title,
+        rating: reviewData.rating,
+        content: reviewData.content,
+        images: reviewData.images,
+      },
+      { new: true }
     );
 
     const recipe = await daoRecipes.findOne({ reviews: reviewId });
 
     if (!recipe) {
-        return { message: "Review does not exist." };
+      return { message: "Review does not exist." };
     }
 
     return { message: "Review updated successfully." };
-}
+  }
 }
